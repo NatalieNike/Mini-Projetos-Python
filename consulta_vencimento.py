3459a51f8c24e3ecd76ef22e2a1465fcfde4b855
Natalie <134658432+NatalieNike@users.noreply.github.com>
2025-03-12 13:06:54 -0300

    Add files via upload

--- consulta_vencimento.py
+++ consulta_vencimento.py
@@ -2,13 +2,16 @@ from datetime import datetime
 
 print("Qual a data de vencimento?")
 
-due_date = input("")
+user_date = input("")
 
 def today():
     today = datetime.now()
     return today
 
-def verify(date_ref):
+def verify_format():
+    print()
+
+def comparison_dates(date_ref):
     if today() > date_ref:
         return True
     else:
