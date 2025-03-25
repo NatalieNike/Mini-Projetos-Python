e578d9b666b2474980666e6cf2402e6ee604f06b
Natalie <134658432+NatalieNike@users.noreply.github.com>
2025-03-25 19:20:05 -0300

    Add files via upload

--- detectar_palindromos.py
+++ detectar_palindromos.py
@@ -1,8 +1,30 @@
+import math
+
 def is_palindrome(word):
     j = len(word)-1
     result = 0
     for i in range(len(word)):
         if word[i] == word[j]:
             result += 1
-       
-        j = j - 1
+        if i >= j:
+            break
+        j -= 1
+
+    if result == math.ceil(len(word)/2):
+        return True
+    else:
+        return False
+    
+#A função recursiva funciona melhor para qualquer tipo de palindromo e é mais otimizada:
+def is_palindrome_recursive(word):
+    if len(word) <= 1:
+        return True
+    else:
+        return word[0] == word[-1] and is_palindrome_recursive(word[1:-1])
+    
+words =  ["arara", "carro", "osso", "natan"]
+
+for word in words:
+    print(word)
+    print(is_palindrome_recursive(word))
+    print("\n")
