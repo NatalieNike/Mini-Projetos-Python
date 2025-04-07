e57a38b85ef8354f6fdae51135caa2a41a6ebaa6
Natalie <134658432+NatalieNike@users.noreply.github.com>
2025-04-07 19:45:17 -0300

    Add files via upload

---
+++ gestao_aquario.py
@@ -0,0 +1,12 @@
+import json
+
+f = open("gestao_aquario/gestao_aquario.json", encoding="utf8")
+
+data_aquarium = json.load(f)
+animals = data_aquarium["data"]
+
+def verify_fish(animal):
+    if animal["type"] == "fish":
+        return True
+    else:
+        return False
