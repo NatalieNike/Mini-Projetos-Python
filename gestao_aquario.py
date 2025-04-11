546ffdae18a51cde39ffc60bb0ba43c967a025fc
Natalie <134658432+NatalieNike@users.noreply.github.com>
2025-04-11 13:40:21 -0300

    Add files via upload

--- gestao_aquario.py
+++ gestao_aquario.py
@@ -5,11 +5,39 @@ f = open("gestao_aquario/gestao_aquario.json", encoding="utf8")
 data_aquarium = json.load(f)
 animals = data_aquarium["data"]
 
+#filter
+
 def verify_fish(animal):
     if animal["type"] == "fish":
         return True
     else:
         return False
     
-    #testetesteteste
-    #testetesteteste
+#retorna os animais do tipo fish
+animals_fish = list(filter(verify_fish, animals))
+print(animals_fish)
+
+#lambda
+
+# Função lambda, é uma forma mais enxuta de escrever a função, sem declará-la, ela não é definida, não pode ser reusada então deve ser usada pontualmente, como no exemplo abaixo:
+# animals_fish = list(filter(lambda animal: (animal["type"] == "fish"), animals))
+
+#map
+
+def animal_name(animal):
+    return animal["name"]
+
+animals_fish_name = list(map(animal_name, animals_fish))
+print(animals_fish_name)
+
+#retorna os nomes dos animais do tipo fish
+
+def assign_to_tank(animals, names_selected, new_tank_number):
+    def change_tank_number(animal):
+        if animal["name"] in names_selected:
+            animal["tank number"] = new_tank_number
+        return animal
+    return list(map(change_tank_number, animals))
+
+new_aquarium = assign_to_tank(animals, animals_fish_name, 42)
+print(new_aquarium)
